84971d22567a921baf6aeb4a71a7da167c13275a
royrajan95 <84740586+royrajan95@users.noreply.github.com>
2023-01-11 00:42:04 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,72 @@
+class Translation(object):
+
+      
+      START_TEXT = """
+🍃 **ʜᴀɪ** __{}__ , 
+
+__I Am Auto CaptionBot Just Add In Channel and See Magic__
+
+**ɪ ᴄᴀɴ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴀᴅᴅ ᴘʀᴇ-sᴇᴛᴛᴇᴅ ᴄᴀᴘᴛɪᴏɴ ᴛᴏ ᴛʜᴇ ғɪʟᴇs ɪɴ ᴄʜᴀɴɴᴇʟs**
+
+__ɪ ᴀᴍ ᴄᴜʀʀᴇɴᴛʟʏ ᴡᴏʀᴋɪɴɢ ғᴏʀ ᴀ ᴄʜᴀɴɴᴇʟ__
+
+😈 __ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ @MoviesLandBackup
+
+"""    
+
+      HELP_TEXT = """
+<u>💡 𝐇𝐞𝐥𝐩</u> 
+
+👉 <i>Cant help...</i>
+
+"""    
+              
+
+     
+      ABOUT_TEXT = """
+📕 **𝐀𝐛𝐨𝐮𝐭 𝐌𝐞**
+
+__○ ᴍʏ ɴᴀᴍᴇ : [Caption Annan](https://t.me/CaptionAnnan_bot)__
+__○ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ __
+__○ ғʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏʀᴏɢʀᴀᴍ __
+__○ sᴇʀᴠᴇʀ : VPS __
+__○ ᴠᴇʀsɪᴏɴ : 2.0.1__
+ 
+"""
+
+      MARKDOWN_TEXT = """
+🔰 <u>𝐀𝐛𝐨𝐮𝐭 𝐌𝐚𝐫𝐤𝐝𝐨𝐰𝐧</u>
+👉 <b>Bold text</b>
+🔸 <code>**markdown text**</code>
+
+👉 <b>Italic text</b>
+🔹 <code>__markdown text__<code> 
+
+👉 <b>Code text</b>
+🔸 <code>`markdown text`</code>   
+
+👉 <b>Hyperlink text</b>
+🔹 <code>[hyperlink_text](https://markdown text)</code> 
+
+〰〰〰〰〰〰〰〰〰〰
+
+"""
+
+# Bot status display
+
+      STATUS_DATA = """
+🔰 <u>𝐁𝐎𝐓 𝐒𝐓𝐀𝐓𝐔𝐒</u>
+
+🖌️ <b>Current Caption :</b> 
+{}
+
+📐 <b>Current Position :</b> {}
+
+"""
+
+
+      SOURCE_TEXT = """
+
+○ <b> I Am Available Open Source on Github </b>
+        
+"""
